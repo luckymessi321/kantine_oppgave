c3fbfba93d6a8b0887b1648f88880ca44af3c1b2
luckymessi321 <luckymessi321@gmail.com>
2025-10-10 15:15:35 +0200

    La til 2 variabler i funksjonen meny med navn day og dager. Variabelen day gir verdien av hvilken ukedag det er i dag med funksjoner fra modulen datetime. Variabelen dager er en ordbok med en nestet ordbok for hver hverdag. Hver av disse nestede ordbøkene har en nøkkel med navnet på dagens rett, og nøkkelverdien er beskrivelsen av retten. Disse variablene skal brukes i malen meny.html

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import datetime
 
 app = Flask(__name__)
 
@@ -8,7 +9,12 @@ def index():
 
 @app.route('/meny')
 def meny():
-    return render_template("meny.html")
+    return render_template("meny.html", day = datetime.datetime.now().strftime("%A"), dager = {"Monday": {"rett": "Pølse", "beskrivelse": "Pølse i brød"},
+                                                                                               "Tuesday": {"rett": "Pizza", "beskrivelse": "Pizza med pepperoni og maur"},
+                                                                                               "Wednesday": {"rett": "Burger", "beskrivelse": "Burger med hundekjøtt"},
+                                                                                               "Thursday": {"rett": "Skorpion", "beskrivelse": "Skorpion med navnet Skarner"},
+                                                                                               "Friday": {"rett": "Ræv", "beskrivelse": "Ræva til moren din"}
+                                                                                               })
 
 @app.route('/kontakt')
 def kontakt():
