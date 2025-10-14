623fe03d99a5afcb79aea9dd59e55099d49aabe7
luckymessi321 <luckymessi321@gmail.com>
2025-10-14 13:24:18 +0200

    Endret def meny slik at dager og days ikke er variabler, men ordbøker i en liste. Denne listen skal brukes i meny.html. La til 2 nye variabler; target_key og target_key2. Disse skal også brukes i meny.html

--- app.py
+++ app.py
@@ -9,12 +9,11 @@ def index():
 
 @app.route('/meny')
 def meny():
-    return render_template("meny.html", day = datetime.datetime.now().strftime("%A"), dager = {"Monday": {"rett": "Pølse", "beskrivelse": "Pølse i brød"},
-                                                                                               "Tuesday": {"rett": "Pizza", "beskrivelse": "Pizza med pepperoni og maur"},
-                                                                                               "Wednesday": {"rett": "Burger", "beskrivelse": "Burger med hundekjøtt"},
-                                                                                               "Thursday": {"rett": "Skorpion", "beskrivelse": "Skorpion med navnet Skarner"},
-                                                                                               "Friday": {"rett": "Ræv", "beskrivelse": "Ræva til moren din"}
-                                                                                               })
+    return render_template("meny.html", day = datetime.datetime.now().strftime("%A"), target_key = "rett", target_key2 = "beskrivelse", list = [{"Monday": "Mandag", "Tuesday": "Tirsdag", "Wednesday": "Onsdag", "Thursday": "Torsdag", "Friday": "Fredag"}, {"Mandag": {"rett": "Pølse", "beskrivelse": "Pølse i brød"},
+                                                                                                                                                                                                                                                               "Tirsdag": {"rett": "Pizza", "beskrivelse": "Pizza med pepperoni og maur"},
+                                                                                                                                                                                                                                                               "Onsdag": {"rett": "Burger", "beskrivelse": "Burger med hundekjøtt"},
+                                                                                                                                                                                                                                                               "Torsdag": {"rett": "Skorpion", "beskrivelse": "Skorpion med navnet Skarner"},
+                                                                                                                                                                                                                                                               "Fredag": {"rett": "Ræv", "beskrivelse": "Ræva til moren din"}}])
 
 @app.route('/kontakt')
 def kontakt():
